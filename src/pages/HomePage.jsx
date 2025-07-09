bbf4ef11f85b65eaa11a068b6296595156e662c1
Idham Ganteng <idham16iramap.com>
2025-07-10 00:04:30 +0700

    update now 10 July 2025

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      
+      <h1>WELCOME</h1>
+      <h1>Home Page Boss!!</h1>
     </div>
   )
 }
 
-export default HomePage
+export default HomePage;
